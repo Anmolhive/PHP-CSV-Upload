9226cc79e000ac566e7ca5ea76d468fbc2a23fd7
Anmolhive <90317789+Anmolhive@users.noreply.github.com>
2022-01-16 11:34:35 +0530

    Update csvUpload.php
    This is simple code to upload CSV data into Mysql Database, you can use this code as Core and Create a Plugin or Apps where one can upload the CSV in Plugin or Apps and it will upload as New Entries or Create a condition that gives the User to Select Between New Entries or Update Old Ones.
    In Any Case, if you Feel like This Code Can Help You. Please Let Me Know.

--- csvUpload.php
+++ csvUpload.php
@@ -12,10 +12,10 @@ foreach ($lines as $line) {
 $arraySize = $i;
 $i = 0;
 $whileRun = $arraySize / 100; //Divide by  100.
-$whileRun = (int) $whileRun;
-$runAfter = $arraySize - $whileRun * 100;
-$run = $arraySize - $runAfter;
-//echo $run."<br>".$runAfter."<br>"; 
+$whileRun = (int) $whileRun; // Number of times While loop gona Run.
+$runAfter = $arraySize - $whileRun * 100; // Remening entries that is smaller than 100 will run after bunch Run.
+$run = $arraySize - $runAfter; // Total entries upload in bunch of 100.
+// Preparing for Upload. 
 $size = 0;
 $ifSize = 0;
 while ($size <= $whileRun + 1) {
@@ -26,13 +26,14 @@ while ($size <= $whileRun + 1) {
         echo "Failed to connect to MySQL: " . $conn->connect_error . "<br>";
         exit();
     }
+    
     if ($ifSize <= $run) {
         for ($forSize = 0; $forSize <= 100; $forSize++)  // Runing Array inside Array.
         {
-            $forCsv = $csv[$i];
+            $forCsv = $csv[$i]; // Preparing data for Upload.
             $sql = "INSERT INTO csvUpload VALUES ('', '$forCsv[0]', '$forCsv[1]')";
 
-            if ($conn->query($sql) === TRUE) {
+            if ($conn->query($sql) === TRUE) { // Uploading Data.
                 echo "New record created successfully till => " . $i . ' From => ' . $arraySize . '<br>';
             } else {
                 echo "Error: " . $sql . "<br>" . $conn->error;
@@ -49,7 +50,7 @@ while ($size <= $whileRun + 1) {
                 $forCsv = $csv[$i];
                 $sql = "INSERT INTO csvUpload VALUES ('', '$forCsv[0]', '$forCsv[1]')";
 
-                if ($conn->query($sql) === TRUE) {
+                if ($conn->query($sql) === TRUE) { // Uploading Remaining Data.
                     echo "New record created successfully till => " . $i . ' From => ' . $arraySize . '<br>';
                 } else {
                     echo "Error: " . $sql . "<br>" . $conn->error;
@@ -59,4 +60,5 @@ while ($size <= $whileRun + 1) {
         }
     }
     $size++;
+    $conn->close(); // Closing Sql Connection.
 }
